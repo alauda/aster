4dc98579749773229b9fa2407fc9791cb2880e3d
bin liu <liubin0329@gmail.com>
2017-03-21 00:11:28 +0800

    add coments and readme

--- README.md
+++ README.md
@@ -4,6 +4,31 @@
 
 ![](docs/images/harujion.png)
 
+# How to use
+
+## Consumer
+
+Please refer to [Examples](examples/) and the comments in the codes.
+
+Create a test topic
+
+```
+$ bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic test-topic --partitions 10 --replication-factor 1
+```
+
+Build and run worker
+
+```
+$ make example
+$ bin/worker
+```
+
+Publish some messages:
+
+```
+$ bin/kafka-console-producer.sh --topic test-topic --broker-list localhost:9092
+```
+
 # License
 
 MIT
--- examples/worker.go
+++ examples/worker.go
@@ -11,64 +11,26 @@ import (
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"os"
 	"os/signal"
-	"strings"
 	"sync"
 	"syscall"
 	"time"
 )
 
 const (
+	// Each consumer should has a name
 	WORKER_NAME = "my-worker"
 )
 
-func main() {
 
-	os.Setenv(common.ENV_KEY_BROKERS, "localhost:9092")
-	os.Setenv(common.ENV_KEY_WORKERS, WORKER_NAME)
 
-	sigChan := make(chan os.Signal)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-
-	logger, _ := logs.SetupLogger(logs.LogConfig{Stdout: true})
-
-	daemon := daemon.NewDaemon(logger)
-	run := true
-
-	if err := daemon.Start(); err != nil {
-		logger.Fatalf("start daemon error: %s", err)
-	}
-
-	for run {
-		select {
-		case sig := <-sigChan:
-			logger.Infof("Caught signal %v: terminating", sig)
-			run = false
-		}
-	}
-
-	// stop daemon
-	daemon.Stop()
-	daemon.Wait()
-	logger.Info("app will exist!")
-
-	logger.Info("app will exist!")
-}
-
-type myProcessor struct {
-	logger    *logrus.Entry
-	mutex     *sync.Mutex
-	closeChan chan bool
-}
-
-func (p *myProcessor) ProcessMessage(message *kafka.Message) error {
-	p.logger.Debugf("Receive: %s", string(message.Value))
-	return nil
-}
-
+// Register this worker to aster at application startup automatically
 func init() {
 	worker.Register(WORKER_NAME, Init)
 }
 
+// Aster will use this func to get a woker instance,
+// which will has more than one worker threads(go routines)
+// This func should implemented by user
 func Init() (*worker.ConsumerWorker, error) {
 
 	brokers := utils.EnvString(common.ENV_KEY_BROKERS, "")
@@ -76,13 +38,14 @@ func Init() (*worker.ConsumerWorker, error) {
 		return nil, fmt.Errorf("No brokers provided")
 	}
 
-	group := utils.EnvString(fmt.Sprintf(common.ENV_KEY_CONSUMER_GROUP, strings.ToUpper(WORKER_NAME)), "test")
-	topic := utils.EnvString(fmt.Sprintf(common.ENV_KEY_TOPIC, strings.ToUpper(WORKER_NAME)), "test")
-	threads := utils.EnvInt64(fmt.Sprintf(common.ENV_KEY_THREADS, strings.ToUpper(WORKER_NAME)), 4)
+	group := "test-group"
+	topic := "test-topic"
+	// Thie worker will has 4 threads run concurrently
+	var threads int64 = 4
 
 	logConf := logs.LogConfig{
 		LogDir:    utils.EnvString(common.ENV_KEY_LOG_DIR, "/var/log/myworker"),
-		AppName:   "test-consumer",
+		AppName:   "test-consumer", // For logging
 		Formatter: logs.DefaultTextFormatter(),
 		Level:     logrus.DebugLevel,
 	}
@@ -93,7 +56,9 @@ func Init() (*worker.ConsumerWorker, error) {
 	if err != nil {
 		return nil, err
 	}
+	// Add tags for logging
 	loggerWithField := logger.WithField("worker", "test-consumer")
+
 	processor := &myProcessor{
 		logger:    loggerWithField,
 		mutex:     &sync.Mutex{},
@@ -103,6 +68,55 @@ func Init() (*worker.ConsumerWorker, error) {
 		processor,
 		loggerWithField)
 	return worker, err
+}
+
+
+// Main function as entry point of an application
+func main() {
+
+	// This two envs will be used by aster worker.
+	os.Setenv(common.ENV_KEY_BROKERS, "localhost:9092")
+	os.Setenv(common.ENV_KEY_WORKERS, WORKER_NAME)
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	logger, _ := logs.SetupLogger(logs.LogConfig{Stdout: true})
+
+	// Use daemon.NewDaemon to create and run a daemon thread
+	daemon := daemon.NewDaemon(logger)
+	run := true
+
+	if err := daemon.Start(); err != nil {
+		logger.Fatalf("start daemon error: %s", err)
+	}
+
+	// Wait a signal to exit app
+	for run {
+		select {
+		case sig := <-sigChan:
+			logger.Infof("Caught signal %v: terminating", sig)
+			run = false
+		}
+	}
+
+	// Stop daemon
+	daemon.Stop()
+	// Wait daemon to exit
+	daemon.Wait()
+	logger.Info("app will exist!")
+}
+
+
+type myProcessor struct {
+	logger    *logrus.Entry
+	mutex     *sync.Mutex
+	closeChan chan bool
+}
+
+func (p *myProcessor) ProcessMessage(message *kafka.Message) error {
+	p.logger.Debugf("Receive: %s", string(message.Value))
+	return nil
 }
 
 func (p *myProcessor) StartDaemon() error {
